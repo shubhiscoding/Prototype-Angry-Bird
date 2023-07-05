6ab460815d343de9b3c2ad682848cd8eba7dae10
Shubh Kesharwani <95687419+drew7wonders@users.noreply.github.com>
2023-07-05 18:06:52 +0530

    Update sketch.js

--- Angry-bird/sketch.js
+++ Angry-bird/sketch.js
@@ -107,19 +107,19 @@ function keyPressed(){
 
 }
 
-async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var responseJSON = await response.json();
+ async function getBackgroundImg(){
+//     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+//     var responseJSON = await response.json();
 
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
+//     var datetime = responseJSON.datetime;
+//     var hour = datetime.slice(11,13);
     
-    if(hour<=0600 && hour>=1900){
-        bg = "sprites/bg2.jpg";
-    }
-    else{
-        bg = "sprites/bg1.png";
-    }
+//     if(hour<=0600 && hour>=1900){
+//         bg = "sprites/bg2.jpg";
+//     }
+//     else{
+//         bg = "sprites/bg1.png";
+//     }
 
     backgroundImg = loadImage(bg);
    // console.log(backgroundImg);
